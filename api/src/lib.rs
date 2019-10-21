13e7161c7adb4295be3932f5aca6579773ef4f96
Takahiro Miyamoto <miyamoto_ta@zoho.com>
2019-10-21 12:13:33 +0900

    Log構造体の定義

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,3 +1,14 @@
+use chrono::{DateTime, Utc};
+use serde_derive::*;
+
+/// JSONの{"user_agent": "xxx", "response_time": 0, "timestamp": "yyyy-MM-dd+HH:mm:ss"}に対応
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Deserialize, Serialize)]
+pub struct Log {
+    pub user_agent: String,
+    pub response_time: i32,
+    pub timestamp: DateTime<Utc>,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
